0aa79e64a5064258f365fa6a2d7de8ec5d766898
you@example.com <you@example.com>
2019-09-18 00:03:01 +0300

    coommit

--- src/components/Bookcase.js
+++ src/components/Bookcase.js
@@ -1,53 +1,58 @@
 import React, { Component } from 'react'
 import Bookshelf from './Bookshelf'
 import { Redirect } from "react-router-dom";
+import PropTypes from 'prop-types';
 
-// add propsTypes
+class Bookcase extends Component {
+  state = {
+    toSearch: false
+  }
 
-class Bookcase extends Component{
-    state = {
-        toSearch: false
-    }
+  componentDidMount() {
+  }
 
-    componentDidMount(){
-    }
-
-    buildShelves = () => 
-        this.props.shelivesTypes.map((selfType,key) => (
-        <Bookshelf 
-            shelfTitle = {selfType}
-            key = {key}
-            classifiedBooks = {this.props.books.filter((b) => (b.shelf === selfType))}
-            onChangeBookShelf= { this.props.onChangeBookShelf }
-        /> 
+  buildShelves = () =>
+    this.props.shelivesTypes.map((selfType, key) => (
+      <Bookshelf
+        shelfTitle={selfType}
+        key={key}
+        classifiedBooks={this.props.books.filter((b) => (b.shelf === selfType))}
+        onChangeBookShelf={this.props.onChangeBookShelf}
+      />
     ));
-    
-    goToSearh = () => this.setState((prevState) => ({ toSearch: true }))
 
-    render() {
+  goToSearh = () => this.setState((prevState) => ({ toSearch: true }))
 
-        if (this.state.toSearch === true){
-            return (<Redirect to='/search' />)
-        }
-        
-        return (
-            <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-              {this.buildShelves()}
-              </div>
-            </div>
-            {/* link */}
-            <div className="open-search">
-             
-              <button onClick={() => this.goToSearh()}>Add a book</button>
-            </div>
-          </div>
-        );
+  render() {
+
+    if (this.state.toSearch === true) {
+      return (<Redirect to='/search' />)
     }
+
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            {this.buildShelves()}
+          </div>
+        </div>
+        {/* link */}
+        <div className="open-search">
+
+          <button onClick={() => this.goToSearh()}>Add a book</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Bookcase
+
+Bookcase.propTypes = {
+  books: PropTypes.array,
+  shelivesTypes: PropTypes.array,
+  onChangeBookShelf: PropTypes.func
+};
